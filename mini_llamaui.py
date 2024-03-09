8caf6627b30f128f385c450b5f7c386ad92f5b28
omnomburp <89351913+omnomburp@users.noreply.github.com>
2024-03-10 00:10:06 +0800

    added chat history
    could be a mistake, originally wrote it so that it wouldn't show history but I found it weird

--- mini_llamaui.py
+++ mini_llamaui.py
@@ -2,21 +2,36 @@ import sys
 import io
 import ollama
 import mistune
+import math
 from PyQt5.QtCore import Qt, pyqtSignal, QThread, QTimer
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QSizePolicy, QScrollArea, QAbstractScrollArea
 
 class MarkdownTextEdit(QTextEdit):
+    def __init__(self):
+        super().__init__()
+
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.textChanged.connect(self.adjustHeight)
+
     def setMarkdown(self, markdown_text):
         html_text = mistune.markdown(markdown_text)
         self.setHtml(html_text)
 
+    def adjustHeight(self):
+        # Get the document size and set the height accordingly
+        new_height = self.document().size().height() + 35  # Add some padding
+        self.setMinimumHeight(math.ceil(new_height))
+        self.setMaximumHeight(math.ceil(new_height))
+
 class LlamaWorker(QThread):
-    llama_finished = pyqtSignal(str)
+    llama_finished = pyqtSignal(str, MarkdownTextEdit)
     llama_ended = pyqtSignal(str)
 
-    def __init__(self, conversation):
+    def __init__(self, conversation, chat_box):
         super().__init__()
         self.conservation = conversation
+        self.chat_box = chat_box
 
     def run(self):
         stream = ollama.chat(
@@ -28,7 +43,7 @@ class LlamaWorker(QThread):
         result_text = ""
         for chunk in stream:
             result_text += chunk["message"]["content"]
-            self.llama_finished.emit(result_text)
+            self.llama_finished.emit(result_text, self.chat_box)
         self.conservation.append({"role": "assistant", "content": result_text})
         self.llama_ended.emit(result_text)
 
@@ -38,7 +53,10 @@ class InputTextEdit(QTextEdit):
     runPython = pyqtSignal()
 
     def keyPressEvent(self, e):
-        if e.key() in (Qt.Key_Enter, Qt.Key_Return):
+        if e.key() == Qt.Key_Return and e.modifiers() == Qt.ShiftModifier:
+            cursor = self.textCursor()
+            cursor.insertText('\n')
+        elif e.key() in (Qt.Key_Enter, Qt.Key_Return):
             if self.toPlainText() == "run":
                 self.setPlainText("")
                 self.runPython.emit()
@@ -62,8 +80,12 @@ class ChatApp(QWidget):
         self.setGeometry(600, 600, 800, 800)
         self.setStyleSheet("background-color: black; color: white;")
 
-        # Create UI elements
-        self.chat_box = MarkdownTextEdit()
+        self.message_input = InputTextEdit()
+        self.message_input.setStyleSheet("background-color: black; color: white;")
+        self.message_input.setAcceptRichText(False)
+
+        self.message_input.setStyleSheet("font-size: 16px;")
+
         style_sheet_vertical = """
                     QScrollBar:vertical {
                         border: none;
@@ -95,28 +117,31 @@ class ChatApp(QWidget):
                         width: 0px;
                     }
                 """
-
-        self.chat_box.setStyleSheet("background-color: black; color: white;")
-        self.chat_box.verticalScrollBar().setStyleSheet(style_sheet_vertical)
-        self.chat_box.horizontalScrollBar().setStyleSheet(style_sheet_horizontal)
-        self.chat_box.setReadOnly(True)
-
-        self.message_input = InputTextEdit()
-        self.message_input.setStyleSheet("background-color: black; color: white;")
-        self.message_input.setAcceptRichText(False)
-
-        self.chat_box.setFontPointSize(16)
-        self.message_input.setStyleSheet("font-size: 16px;") # using this will make placeholder text bigger as well
+        
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.verticalScrollBar().setStyleSheet(style_sheet_vertical)
+        scroll_area.horizontalScrollBar().setStyleSheet(style_sheet_horizontal)
+        
+        self.message_input.setPlaceholderText("type here...")
 
         # Create layout
         layout = QVBoxLayout()
-        layout.addWidget(self.chat_box)
+        layout.setSpacing(5)
+        layout.addWidget(scroll_area)  # Add the scroll area to the main layout
         layout.addWidget(self.message_input)
 
-        layout.setStretchFactor(self.chat_box, 9)
+        layout.setStretchFactor(scroll_area, 9)
         layout.setStretchFactor(self.message_input, 1)
-        self.message_input.setPlaceholderText("type here...")
-        self.message_input.setFocus()
+
+        self.chat_layout = QVBoxLayout()
+        self.chat_layout.setSpacing(5)
+        self.chat_layout.setContentsMargins(3, 5, 3, 5)
+        self.chat_layout.setAlignment(Qt.AlignTop)
+
+        central_widget = QWidget()
+        central_widget.setLayout(self.chat_layout)
+        scroll_area.setWidget(central_widget)
 
         # Set layout
         self.setLayout(layout)
@@ -132,16 +157,33 @@ class ChatApp(QWidget):
     def minimize_window(self):
         self.showMinimized()
 
+    def create_markdown_widget(self) -> MarkdownTextEdit:
+        # Create UI elements
+        chat_box = MarkdownTextEdit()
+        chat_box.setStyleSheet("background-color: black; color: white; font-size: 16px;")
+        chat_box.setFontPointSize(16)
+        chat_box.setReadOnly(True)
+        chat_box.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        chat_box.setContentsMargins(3, 5, 3, 5)  # Adjust these values as needed
+        return chat_box
+
     def send_message(self):
         # Get message from input
         message = self.message_input.toPlainText()
+        chat_box = self.create_markdown_widget()
+        chat_box.setStyleSheet("color: #9370DB; font-size: 16px;")
+        self.chat_layout.addWidget(chat_box)
+        chat_box.setMarkdown(message)
         self.conversation.append({"role": "user", "content": message})
         self.message_input.clear()
         self.message_input.setDisabled(True)
         self.send_llama()
 
     def send_llama(self):
-            llama_worker = LlamaWorker(self.conversation)
+            chat_box = self.create_markdown_widget()
+            # append to the new vlayout
+            self.chat_layout.addWidget(chat_box)
+            llama_worker = LlamaWorker(self.conversation, chat_box)
             llama_worker.llama_finished.connect(self.llama_finished)
             llama_worker.llama_ended.connect(self.llama_ended)
             llama_worker.setParent(self)
@@ -151,16 +193,16 @@ class ChatApp(QWidget):
             self.process_events_timer.timeout.connect(QApplication.processEvents)
             self.process_events_timer.start(100)
 
-    def llama_finished(self, result_text):
-        self.chat_box.setMarkdown(result_text)
-        scroll_bar = self.chat_box.verticalScrollBar()
-        scroll_bar.setValue(scroll_bar.maximum())
+    def llama_finished(self, result_text, chat_box):
+        chat_box.setMarkdown(result_text)
 
     def llama_ended(self, output_text):
         # should probably add user confirmation
         self.message_input.setDisabled(False)
         if "```python\n" in output_text:
-            self.chat_box.append("Python code detected") 
+            chat_box = self.create_markdown_widget()
+            self.chat_layout.addWidget(chat_box)
+            chat_box.setMarkdown("Python code detected, type run to run the code\n")
             python_code = output_text.split('```python\n')[1].split("```")[0]
             self.python_code = python_code
 
@@ -170,7 +212,9 @@ class ChatApp(QWidget):
         exec(self.python_code)
         sys.stdout = sys.__stdout__
         output_result = captured_output.getvalue()
-        self.chat_box.append("Code Output:\n" + output_result)
+        chat_box = self.create_markdown_widget()
+        self.chat_layout.addWidget(chat_box)
+        chat_box.setMarkdown("Code Output:\n" + output_result + "\n")
             
 if __name__ == '__main__':
     app = QApplication(sys.argv)
